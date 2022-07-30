32fbf464967b2747a630a468d5da88c29b7840b9
berserkersword <lamjarred063@gmail.com>
2022-07-30 08:05:02 -0400

    fix-converter

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <nav className="bg-transparent backdrop-blur-2xl px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600" style={{ maxHeight: '20vh' }}>
             <div className="container flex flex-wrap justify-between items-center mx-auto">
                 <a href="https://flowbite.com/" className="flex items-center">
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Currency</span>
                 </a>
                 <div className="flex md:order-2">
                     <button onClick={() => setCollapse(!collapse)} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
